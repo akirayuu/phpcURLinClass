c6e923c0997261a68711e7964cb168d8e5a0a63f
Fatini Lidya <fatinilidya@gmail.com>
2021-12-21 15:22:42 +0800

    Create curl_in_class.php
    Create curl_in_class

---
+++ curl_in_class.php
@@ -0,0 +1,42 @@
+<?php
+
+/* Create class name ListofStudent */
+class ListofStudent {
+    /* Declare variable name student */
+    private $student;
+    private $url;
+       
+    /* Create contructor */
+    function __contruct(){
+        /* contructor */
+        $this->url = 'https://reqres.in/api/users?page=2';
+
+        /* Initialize cURL object with handle */
+    $this->student = curl_init();
+    curl_setopt($this->student, CURLOPT_URL, $this->url); //fetch data from url
+    curl_setopt($this->student, CURLOPT_RETURNTRANSFER, 1); //1 = true
+    
+        /* Respond in string and send data to student */
+    $resp = curl_exec($this->student);
+
+    /* Error handler */
+    if ($e = curl_error($this->student)) {
+        echo $e;
+    }
+    else {
+        $decode = json_decode($resp);
+        print_r($decode);
+       }
+    }
+    
+    /* Destructor */
+    function __destruct()    {
+        if ($this->student)
+        {
+            /* Close cURL handle */
+            curl_close($this->student);
+            $this->student = NULL;
+        }
+    }
+}
+?>
